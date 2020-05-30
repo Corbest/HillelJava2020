39ed6c35e37adf35549c6bc45c75e804531ed327
Corbest <lord.vader.1997@gmail.com>
2020-05-30 22:13:08 +0300

    HM21 DB upload v1.0

--- src/main/java/HM19(21)/DataBase.sql
+++ src/main/java/HM19(21)/DataBase.sql
@@ -1,32 +1,31 @@
-Create database Hillel2020;
 
-create table students
+create table `hillel2020`.`Students`
 (
     Student_ID int             not null
     auto_increment    primary key,
     Full_Name  varchar(255)   not null,
-    IdGroup    int  not null,
+    Group_ID    int  not null,
     HowJoin    year            not null,
-    foreign key (IdGroup) references team (Group_ID)
+    foreign key (Group_ID) references `Students`.`team`(Group_ID)
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-create table team
+create table `Students`.team
 (
     Group_ID int not null
         auto_increment primary key,
     InGroup    enum ('Elementary') not null
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-create table assessment
+create table`Students`.assessment
 (
  Assessment_ID bigint not null auto_increment primary key,
  Lesson_ID int not null,
  Student_ID int not null,
- foreign key (Lesson_ID) references lessons(Lesson_ID),
+ foreign key (Lesson_ID) references`assessment`.lessons(Lesson_ID),
  foreign key (Student_ID) references students(Student_ID)
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-create table lessons
+create table `assessment`.lessons
 (
     Lesson_ID int not null auto_increment primary key ,
     Name_of_lesson enum ('Mathematics','
@@ -34,18 +33,18 @@ Programming','Work') not null ,
     Teacher_ID int not null,
     Semester int not null,
     Year year,
-    foreign key (Teacher_ID) references teachers(Teacher_ID)
+    foreign key (Teacher_ID) references`lessons`.teachers(Teacher_ID)
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-create table teachers
+create table`lessons`.`teachers`
 (
     Teacher_ID int not null auto_increment primary key,
     Name_of_teacher varchar(255) not null ,
     Department_ID int not null,
-    foreign key (Department_ID) references  Department(Department_ID)
+    foreign key (Department_ID) references`teachers`.Department(Department_ID)
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-create table Department
+create table`teachers`.Department
 (
     Department_ID int not null auto_increment primary key,
     Name_of_department enum ('Mathematics','Programming','Work'),

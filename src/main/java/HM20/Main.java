e448f9567a87d1efa9803e98f26ff6203ca7dfa2
Corbest <lord.vader.1997@gmail.com>
2020-06-04 12:47:58 +0300

    HM22 DB

--- src/main/java/HM20/Main.java
+++ src/main/java/HM20/Main.java
@@ -11,13 +11,40 @@ public class Main {
                         + "useSSL=false&user=root&password=0481Ruslan");
 
                 Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery("select * from student.student");
-        System.out.println(rs.getMetaData().getColumnName(1));
+
+                //Все втуденты
+//        ResultSet rs = statement.executeQuery("SELECT * FROM `student`;");
+//        while (rs.next()) {
+//            System.out.println("Id: "+rs.getInt(1));
+//            System.out.println("Name: "+rs.getString(2));
+//            System.out.println("Year: "+rs.getInt(3));
+//            System.out.println("Group Id: "+rs.getInt(4));
+//            System.out.println("====================");
+//        }
+
+
+        //Все студенты учащиеся в одной группе
+//        ResultSet rs2 = statement.executeQuery("SELECT * FROM `student` where Group_ID = 1 ");
+//        while (rs2.next()) {
+//            System.out.println("Id: "+rs2.getInt(1));
+//            System.out.println("Name: "+rs2.getString(2));
+//            System.out.println("Year: "+rs2.getInt(3));
+//            System.out.println("Group Id: "+rs2.getInt(4));
+//            System.out.println("====================");
+//        }
+
+        //Все студенты одного года поступления
+        ResultSet rs3 = statement.executeQuery("SELECT * FROM `student` where HowJoin = 2019");
+        while (rs3.next()) {
+            System.out.println("Id: "+rs3.getInt(1));
+            System.out.println("Name: "+rs3.getString(2));
+            System.out.println("Year: "+rs3.getInt(3));
+            System.out.println("Group Id: "+rs3.getInt(4));
+            System.out.println("====================");
+        }
         connection.close();
-        System.out.println();
     }
 }
-
 
 
 
